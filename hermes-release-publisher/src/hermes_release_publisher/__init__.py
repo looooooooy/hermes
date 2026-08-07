263181bd627d475855c2e3012f8dc2a6d9e98397
looooooooy <105577763+looooooooy@users.noreply.github.com>
2026-08-07 22:49:01 +0800

    feat(update): expose release publisher API

---
+++ hermes-release-publisher/src/hermes_release_publisher/__init__.py
@@ -0,0 +1,23 @@
+from .repository import (
+    BucketMap,
+    ObjectAlreadyExists,
+    OssV2Backend,
+    PublishReceipt,
+    PublisherError,
+    ReleasePublisher,
+    RemoteObject,
+    UploadResult,
+    content_addressed_key,
+)
+
+__all__ = [
+    "BucketMap",
+    "ObjectAlreadyExists",
+    "OssV2Backend",
+    "PublishReceipt",
+    "PublisherError",
+    "ReleasePublisher",
+    "RemoteObject",
+    "UploadResult",
+    "content_addressed_key",
+]
